e0ed19815922b9dbbbaab69f5e461194551ccf8a
Sergey Spiridovich <raboch2014@mail.ru>
2024-01-13 04:36:26 +0300

    Задание №8: Научиться запускать тесты в разных браузерах

--- src/test/java/manager/ApplicationManager.java
+++ src/test/java/manager/ApplicationManager.java
@@ -3,6 +3,7 @@ package manager;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
     private LogHelper session;
@@ -11,13 +12,22 @@ public class ApplicationManager {
     protected WebDriver driver;
 
     @BeforeEach
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            driver = new ChromeDriver();
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            }
+            if (driver == null) {
+                if ("chrome".equals(browser)) {
+                    driver = new ChromeDriver();
+                } else {
+                    throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+                }
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http://localhost/addressbook/");
             driver.manage().window().setSize(new Dimension(968, 728));
-            session().login("admin","secret");
+            session().login("admin", "secret");
         }
     }
 
