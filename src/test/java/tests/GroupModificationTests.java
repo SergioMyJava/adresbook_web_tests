076f496be5aa50125efb5e2691ce9122d0f812b6
Sergey Spiridovich <raboch2014@mail.ru>
2024-01-28 23:43:11 +0300

    Задание №11: Сделать тесты для модификации групп и модификации контактов

--- src/test/java/tests/GroupModificationTests.java
+++ src/test/java/tests/GroupModificationTests.java
@@ -1,15 +1,33 @@
 package tests;
 
 import model.GroupData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
 
 public class GroupModificationTests extends TasteBase {
 
     @Test
     void canModifayGroup() {
-        if (!app.getGroupHelper().isGroupPresent()) {
-            app.getGroupHelper().createGroup(new GroupData("", "", ""));
+        if (app.getGroupHelper().getCount() == 0) {
+            app.getGroupHelper().createGroup(new GroupData("", "", "", ""));
         }
-        app.getGroupHelper().modifyGroup(new GroupData().withName("ModifyName"));
+        var oldGroups = app.getGroupHelper().getList();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldGroups.size());
+        var modifyGroup = new GroupData().withName("ModifyName");
+        app.getGroupHelper().modifyGroup(oldGroups.get(index), modifyGroup);
+        var newGroups = app.getGroupHelper().getList();
+        var expectedList = new ArrayList<>(oldGroups);
+        expectedList.set(index,modifyGroup.withId(oldGroups.get(index).id()));
+        Comparator<GroupData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newGroups.sort(compareById);
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newGroups, expectedList);
     }
 }
--- src/test/java/tests/UserModificationTests.java
+++ src/test/java/tests/UserModificationTests.java
@@ -1,17 +1,42 @@
 package tests;
 
 import model.UserData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
 
 public class UserModificationTests extends TasteBase {
 
     @Test
-    void canModifayGroup() {
-        if (!app.getUserHelper().isUserPresent()) {
-            app.getUserHelper().createUserInAdressbook(new UserData("", "", "", "", "", "", "", "", "", "",
-                    "", "", "", "", "", ""));
+    void canModifayUser() throws InterruptedException {
+        if (app.getUserHelper().getCountUser() == 0) {
+            app.getUserHelper().createUserInAdressbook(new UserData("", "",
+                    "", "", "", "", "", "",
+                    "", "", "","", "", "", "",
+                    "", ""));
         }
-        app.getUserHelper().modifyUser(new UserData().userWithFullNameAdressMobile("ModifyName", "ModifyMiddleName",
-                "ModifyLastname", "Modify addres", "Modify mobile (800)345-54-56"));
+        var oldUsersList = app.getUserHelper().getList();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldUsersList.size());
+        var modifayUser = new UserData().userWithFullNameAdressMobile("Lavon",
+                "Volsky","s.Baukov","+900-345-567");
+        app.getUserHelper().modifyUser(oldUsersList.get(index),modifayUser);
+        Thread.sleep(3000);
+        var newUsersList = app.getUserHelper().getList();
+        var expectedList = new ArrayList<>(oldUsersList);
+
+        expectedList.set(index,modifayUser.withId(oldUsersList.get(index).id()));
+
+        Comparator<UserData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newUsersList.sort(compareById);
+        expectedList.sort(compareById);
+
+        Assertions.assertEquals(newUsersList, expectedList);
     }
-}
+
+    }
+
